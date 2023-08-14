184b1bb01c6588e5970c3004dd2629728629b610
aditya917 <52827225+aditya917@users.noreply.github.com>
2023-08-14 15:46:20 -0700

    project2 completed

--- Chapter 1 Projects/project2.cpp
+++ Chapter 1 Projects/project2.cpp
@@ -2,10 +2,29 @@
 using namespace std;
 
 int main(){
+    const double ARTIFICIAL_SWEETENER = 0.001;
+
     double amountToKillMouse;
     double weightOfMouse;
     double weightOfDieterStopping;
-    double weight;
+    double amountToKillHuman;
+
+    double dietSodas;
+
+    cout << "Artificial mouse needed to kill a mouse: ";
+    cin >> amountToKillMouse;
+
+    cout << "Weight of the mouse: ";
+    cin >> weightOfMouse;
+
+    cout << "Weight of the dieter stopping: ";
+    cin >> weightOfDieterStopping;
+
+    amountToKillHuman = (weightOfDieterStopping*amountToKillMouse)/weightOfMouse;
+
+    dietSodas = amountToKillHuman/ARTIFICIAL_SWEETENER;
+
+    cout << "It would take " << dietSodas << " to kill a human." << endl;
 
     return 0;
 }
